ea782a67c384bc5061048dcb2f73b495e33aec30
Salman Ahmad <i210469@nu.edu.pk>
2025-02-19 03:54:22 +0500

    Hope it works

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/wedding-website/', // Add this line - should match your repository name
+  base: './' // Change this from '/wedding-website/' to './'
 })
